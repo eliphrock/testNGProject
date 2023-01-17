68932405b2412e212cbe91edfa6b6880bfc99fd3
eliphrock <eliphrock@gmail.com>
2023-01-16 21:42:51 -0500

    tests

--- src/test/java/techproed/tests/smoketest/logintests/Day19_NegativeLoginTest.java
+++ src/test/java/techproed/tests/smoketest/logintests/Day19_NegativeLoginTest.java
@@ -16,7 +16,7 @@ public class Day19_NegativeLoginTest {
     HomePage homePage;
     LogInPage loginPage;
     Faker faker;
-    @Test
+    @Test(priority = 1,groups = "minor-regression-group")
     public void US100208_Negative_Login() throws IOException {
 //        As customer, I should not be able to log in the application
         //going blue rental car home page
